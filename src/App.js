de63167f3a7d9aece8e38bae68f1591f0179f858
yutabee <bsk87m@gmail.com>
2022-07-22 21:57:39 +0900

    router setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import { Home } from './components/Home';
+import { CreatePost } from './components/CreatePost';
+import { Login } from './components/Login';
+import { Logout } from './components/Logout';
 
 function App() {
   return (
-    <div>
-      Hello react
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/createpost' element={<CreatePost />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
